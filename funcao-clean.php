8aa49ff92f8e1e25f0a7d14b099026a3a2848b75
Eduardo Honorato <eduardo_pk@live.com>
2019-05-31 13:40:26 -0300

    Create funcao-clean.php

---
+++ funcao-clean.php
@@ -0,0 +1,38 @@
+<?
+function clean($string)
+{
+    $table = array(
+        'Š'=>'S', 'š'=>'s', 'Đ'=>'Dj', 'đ'=>'dj', 'Ž'=>'Z',
+        'ž'=>'z', 'Č'=>'C', 'č'=>'c', 'Ć'=>'C', 'ć'=>'c',
+        'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A',
+        'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+        'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I',
+        'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O',
+        'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U',
+        'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss',
+        'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a',
+        'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e',
+        'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i',
+        'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o',
+        'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u',
+        'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b',
+        'ÿ'=>'y', 'Ŕ'=>'R', 'ŕ'=>'r', '-'=>'',
+    );
+    // Traduz os caracteres em $string, baseado no vetor $table
+    $string = trim($string);
+    $string = strtr($string, $table);
+    // converte para minúsculo
+    $string = strtolower($string);
+    // remove caracteres indesejáveis (que não estão no padrão)
+    $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
+    // Remove múltiplas ocorrências de hífens ou espaços
+    $string = preg_replace("/[\s-]+/", " ", $string);
+    // Transforma espaços e underscores em hífens
+    $string = preg_replace("/[\s_]/", "-", $string);
+        if (substr($string, -1) == '-') $string = substr($string, 0, -1);
+
+    // retorna a string
+    return $string;
+
+}
+?>
